9abb5a46ad343b9c8c9a80f3792d1d6ad00793f3
Aleksey <d_alesha@rambler.ru>
2025-12-10 16:57:02 +0300

    add class

---
+++ src/main/java/org/skypro/skyshop/model/product/DiscountedProduct.java
@@ -0,0 +1,44 @@
+package org.skypro.skyshop.model.product;
+
+
+import java.util.UUID;
+
+public class DiscountedProduct extends Product {
+    private final int baseAmount;
+    private final int discount;
+    private final UUID id;
+
+    public DiscountedProduct( UUID id, String name, int baseAmount, int discount) {
+        super(name);
+        this.id = id;
+        if (baseAmount < 1) {
+            throw new IllegalArgumentException("Цена должна быть больше 0");
+        }
+
+        this.baseAmount = baseAmount;
+        if (discount < 0 || discount > 100) {
+            throw new IllegalArgumentException("Некорректное значение.Скидка может быть в интервале от 0 до 100");
+        }
+        this.discount = discount;
+    }
+
+    @Override
+    public UUID getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isSpecial() {
+        return true;
+    }
+
+    @Override
+    public int getPrice() {
+        return baseAmount - baseAmount * discount / 100;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "<" + getPrice() + "> (<" + discount + ">%)";
+    }
+}
---
+++ src/main/java/org/skypro/skyshop/model/product/Product.java
@@ -0,0 +1,57 @@
+package org.skypro.skyshop.model.product;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.skypro.skyshop.model.search.Searchable;
+
+import java.util.Objects;
+
+
+    public abstract class Product implements Searchable {
+        protected final String TERM_PRODUCT = "PRODUCT";
+        private final String name;
+
+        public Product(String name) {
+            if (("null" == name) || name.isBlank()) {
+                throw new IllegalArgumentException("Поле Имя не может быть пустым");
+            }
+            this.name = name;
+        }
+
+        public abstract boolean isSpecial();
+
+        public abstract int getPrice();
+
+        @Override
+        public String getName() {
+            return name;
+        }
+        @JsonIgnore
+        @Override
+        public String searchTerm() {
+            return getName();
+        }
+        @JsonIgnore
+        @Override
+        public String getContent() {
+            return TERM_PRODUCT;
+        }
+
+        @Override
+        public String toString() {
+            return "<" + name + ">:";
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (this == other) return true;
+            if (other == null) return false;
+            Searchable product = (Searchable) other;
+            return Objects.equals(name, product.getName());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(name);
+        }
+
+    }
